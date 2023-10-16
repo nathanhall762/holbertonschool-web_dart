0144c979c38f273373f9f2baf7a457e7ca3a72a7
nathanhall762 <nathanhall762@gmail.com>
2023-10-16 13:36:40 -0500

    task 4

--- 0x02-dart-classes/4-mutables.dart
+++ 0x02-dart-classes/4-mutables.dart
@@ -7,7 +7,7 @@ class Password {
 
 
   String toString() {
-    return "Your Password: $_password";
+    return "Your Password is: $_password";
   }
 
   String get password {
@@ -17,7 +17,7 @@ class Password {
   set password(String newPassword) {
     _password = newPassword;
   }
-  
+
   bool isValid() {
     return _password.length >= 8 && _password.length <= 16 && _password.contains(RegExp(r'[A-Z]')) && _password.contains(RegExp(r'[a-z]')) && _password.contains(RegExp(r'[0-9]'));
   }
