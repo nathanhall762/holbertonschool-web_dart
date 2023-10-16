1e15deb6b80f9ade724b19fdd91f28a73670d888
nathanhall762 <nathanhall762@gmail.com>
2023-10-16 12:58:23 -0500

    task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,9 +1,9 @@
 void outer(String name, String id) {
   String inner() {
-    String firstInitial = name[0];
     List<String> nameList = name.split(" ");
-    String lastName = nameList[1];
-    return "Hello Agent $firstInitial.$lastName your id is $id";
+    String lastInitial = nameList[1][0];
+    String firstName = nameList[0];
+    return "Hello Agent $lastInitial.$firstName your id is $id";
   }
 
   print(inner());
